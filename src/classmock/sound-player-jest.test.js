eab24cbaa044d369b036585994ec57b592ad8b8d
Alphonse Bendt <alphonse.bendt@gmail.com>
2018-05-26 16:03:47 +0200

    flow lint

--- src/classmock/sound-player-jest.test.js
+++ src/classmock/sound-player-jest.test.js
@@ -1,18 +1,16 @@
-// @flow 
-
-import type {JestMockFn} from "jest"; // eslint-disable-line
+// @flow
 
 import SoundPlayer from './sound-player';
 
 jest.mock('./sound-player'); // SoundPlayer is now a mock constructor
 
-function _mock<T>(mockFn: T): JestMockFn {
-    return ((mockFn: any): JestMockFn);
+function _mock<T>(mockFn: T): JestMockFn<Array<any>,any>  {
+    return ((mockFn: any): JestMockFn<Array<any>,any>) ;
 }
 
 beforeEach(() => {
   // Clear all instances and calls to constructor and all methods:
-  _mock(SoundPlayer).mockClear();
+   _mock(SoundPlayer).mockClear();
 });
 
 it('We can check if the consumer called the class constructor', () => {
